69a4b0a717ac5b6c3959d6924e59a9d32e26cb5e
georgespackman <[emxghs@nottingham.ac.uk]>
2021-05-25 20:20:19 +0100

    Adding setbinderposition function

--- Core/TextileDecoupled.cpp
+++ Core/TextileDecoupled.cpp
@@ -28,4 +28,41 @@ void CTextileDecoupled::PopulateTiXmlElement(TiXmlElement &Element, OUTPUT_TYPE 
 
 void CTextileDecoupled::SetBinderPosition(int x, int y, vector<int> zOffsets)
 {
+	vector<PATTERN3D> &Cell = GetCell(x, y);
+	int size = Cell.size();
+	vector<int>::iterator itzOffsets;
+	int BinderPos;
+	vector<int> BinderPositions;
+
+	for (itzOffsets = zOffsets.begin(); itzOffsets != zOffsets.end(); itzOffsets++)
+	{
+		BinderPos = (size - 1) - *itzOffsets * 2;
+		BinderPositions.push_back(BinderPos);
+	}
+
+	/*int MaxBinderPos = (size - 1) - (zOffsets * 2);   // *2 because each offset shifts by two cell positions
+	int MinBinderPos = (size - 1) - (zOffsets + m_iNumBinderLayers - 1) * 2;
+	if (MinBinderPos < 0)
+	{
+		TGERROR("Unable to set binder positions, lowest binder position too low for number of weft yarns");
+		return;
+	}
+	if (MaxBinderPos > size - 1)
+	{
+		TGERROR("Unable to set binder positions, upper binder position too high for number of weft yarns");
+		return;
+	}*/
+
+	// Set x yarn in each of positions between Max and Min binder pos
+	for (int j = 0; j < size; j += 2)
+	{
+		if (count(BinderPositions.begin(), BinderPositions.end(), j ))
+		{
+			Cell[j] = PATTERN3D_XYARN;
+		}
+		else
+		{
+			Cell[j] = PATTERN3D_NOYARN;
+		}
+	}
 }
